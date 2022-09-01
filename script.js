be308908fee0080e525cf257c2396449e80265f5
parvingasimzade <gasimzadeparvin@gmail.com>
2022-09-01 15:49:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+
+        label.innerHTML = label.innerText
+                .split('')
+                .map((letter,idx) => `<span style="transition-delay:${idx * 70}ms">${letter}</span>`)
+                .join('')
+})
